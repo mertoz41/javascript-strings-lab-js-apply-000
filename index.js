064a68735a787fcc268512391282e2a9ae090910
Mert  Ozkaynak <mertozkaynak@windowslive.com>
2020-02-25 20:05:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Neil deGrasse Tyson"
 
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
-var topic = "Let\'s";
+var topic = "Let\'s talk about space.";
 
-var conversation = `${topic} talk about space.`;
+var conversation = topic;
